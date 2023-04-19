37a3073382d80c236704cbc9a769d81a84199d45
raquan nichols <Rnichols@appdev.yearup.org>
2023-04-19 15:56:59 -0400

    Intial Commit

---
+++ src/main/java/com/yearup/CellPhoneApp.java
@@ -0,0 +1,10 @@
+package com.yearup;
+
+public class CellPhoneApp {
+    public static void main(String[] args) {
+        CellPhone phone = new CellPhone("12345", "Sprint","551-222-2222", "Raquan", "Iphone 11");
+        System.out.printf("%s belongs to %s", phone.phoneNumber,phone.owner);
+
+    }
+}
+
